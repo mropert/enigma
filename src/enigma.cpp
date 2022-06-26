de46a5bca38ee8e164453fdea7abc11cb7e1772c
Mathieu Ropert <mro@puchiko.net>
2022-06-26 17:48:29 +0200

    Remove more modulos. Don't use string append to generate output

--- src/enigma.cpp
+++ src/enigma.cpp
@@ -37,7 +37,7 @@ m4_machine::m4_machine( const std::array<rotor, 4>& rotors,
 std::string m4_machine::decode( std::string_view message, std::string_view key )
 {
 	std::string result;
-	result.reserve( message.size() );
+	result.resize( message.size(), 'A' );
 
 	const std::array<char, 4> start_positions = { key[ 0 ] - 'A', key[ 1 ] - 'A', key[ 2 ] - 'A', key[ 3 ] - 'A' };
 
@@ -46,15 +46,16 @@ std::string m4_machine::decode( std::string_view message, std::string_view key )
 								   start_positions[ 2 ] - m_rings_settings[ 2 ],
 								   start_positions[ 3 ] - m_rings_settings[ 3 ] };
 
+	auto output = begin( result );
 	for ( const auto character : message )
 	{
 		char input = character;
 
-		if ( m_rotors[ 3 ].m_turnovers[ 0 ] == ( offsets[ 3 ] % 26 ) || m_rotors[ 3 ].m_turnovers[ 1 ] == ( offsets[ 3 ] % 26 ) )
+		if ( m_rotors[ 3 ].m_turnovers[ 0 ] == offsets[ 3 ] || m_rotors[ 3 ].m_turnovers[ 1 ] == offsets[ 3 ] )
 		{
 			offsets[ 2 ] = ( offsets[ 2 ] + 1 ) % 26;
 		}
-		else if ( m_rotors[ 2 ].m_turnovers[ 0 ] == ( offsets[ 2 ] % 26 ) || m_rotors[ 2 ].m_turnovers[ 1 ] == ( offsets[ 2 ] % 26 ) )
+		else if ( m_rotors[ 2 ].m_turnovers[ 0 ] == offsets[ 2 ] || m_rotors[ 2 ].m_turnovers[ 1 ] == offsets[ 2 ] )
 		{
 			offsets[ 2 ] = ( offsets[ 2 ] + 1 ) % 26;
 			offsets[ 1 ] = ( offsets[ 1 ] + 1 ) % 26;
@@ -80,7 +81,7 @@ std::string m4_machine::decode( std::string_view message, std::string_view key )
 
 		input = m_plugboard[ input - 'A' ];
 
-		result += input;
+		*output++ = input;
 	}
 	return result;
 }
